e928dbdedb4859c5d4f87017533f57b1d432ccad
ahmed-malek-TLILI <ahmedmalek.tlili@ensi-uma.tn>
2023-12-14 19:46:17 +0100

    Initializing SpringBoot Project

---
+++ src/test/java/com/example/springbootlili/AhmedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhmedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
